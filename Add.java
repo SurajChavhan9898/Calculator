98163de2c70806301ceb62ab7d32608fac62180c
SurajChavhan9898 <105916294+SurajChavhan9898@users.noreply.github.com>
2022-06-12 19:44:02 +0530

    Add.java

---
+++ Add.java
@@ -0,0 +1,19 @@
+
+public class Add implements Operate {
+
+    @Override
+    public Double getResult(Double... numbers) {
+        Double sum = 0.0;
+
+        for(Double num : numbers) {
+            sum += num;
+        }
+
+        return sum; 
+    }
+}
+
+
+
+
+/**************End of Third Package **************/
